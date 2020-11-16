705e11aeea989e6e582e856c851142596ba8628c
Cyrielle Gallou <cyrielle.gallou@gmail.com>
2020-11-16 17:41:03 +0100

    update architecture

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AProposComponent } from './public/a-propos/a-propos.component';
+import { BijouxComponent } from './public/bijoux/bijoux.component';
+import { ConnexionComponent } from './public/connexion/connexion.component';
 import { HomeComponent } from './public/home/home.component';
+import { MentionsLegalesComponent } from './public/mentions-legales/mentions-legales.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'bijoux', component: BijouxComponent },
+  { path: 'a-propos', component: AProposComponent },
+  { path: 'connexion', component: ConnexionComponent },
+  { path: 'mentions-legales', component: MentionsLegalesComponent },
+
 ];
 
 @NgModule({
