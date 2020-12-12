f2a246210508a040e6ed97291f4c5df7aa2ef1c5
CyrielleGl <cyrielle.gallou@gmail.com>
2020-12-12 13:32:35 +0100

    upgrade angular version & add ngb-tooltip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +14,6 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    NgbModule,
     AppRoutingModule,
     CoreModule,
     SharedModule
