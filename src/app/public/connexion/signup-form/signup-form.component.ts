1612556f1e91e4009af1a44607b93c5080e548c0
Hugo Fief <hfief1806@gmail.com>
2021-01-14 20:46:38 +0100

    ajout service authentification + signup need to fix

--- src/app/public/connexion/signup-form/signup-form.component.ts
+++ src/app/public/connexion/signup-form/signup-form.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { AuthService } from "../../../shared/services/api/auth/auth.service";
 
 @Component({
-  selector: 'app-signup-form',
-  templateUrl: './signup-form.component.html',
-  styleUrls: ['./signup-form.component.scss']
+  selector: "app-signup-form",
+  templateUrl: "./signup-form.component.html",
+  styleUrls: ["./signup-form.component.scss"],
 })
 export class SignupFormComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  public data: any;
+  public submitted: boolean = false;
 
-  ngOnInit(): void {
+  Form = new FormGroup({
+    id: new FormControl(null),
+    lastname: new FormControl("", [Validators.required]),
+    firstname: new FormControl("", [Validators.required]),
+    mail: new FormControl("", [Validators.required]),
+    password: new FormControl("", [Validators.required]),
+    confirmpassword: new FormControl("", [Validators.required]),
+  });
+
+  ngOnInit(): void {}
+
+  public registration(): void {
+    this.submitted = true;
+
+    if (this.Form.invalid) {
+      return;
+    }
+
+    console.log(this.Form.value);
+
+    this.authService
+      .registration(this.Form.value)
+      .subscribe((data: string[]) => {
+        this.data = data;
+      });
   }
-
 }
--- src/app/shared/services/api/api.service.ts
+++ src/app/shared/services/api/api.service.ts
@@ -1,6 +1,8 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable()
 export class ApiService {
-  public static readonly ApiUrlUsers: string = 'http://localhost:4000/api/users';
+  public static readonly ApiUrlUsers: string =
+    "http://localhost:4000/api/users";
+  public static readonly ApiUrlAuth: string = "http://localhost:4000/api/auth";
 }
