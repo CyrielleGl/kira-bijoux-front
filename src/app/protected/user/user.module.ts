fe6d9a718acd39ae0d8020f4a3cf54e75f3019f2
Hugo Fief <hfief1806@gmail.com>
2021-01-15 23:30:56 +0100

    updated single quotes

--- src/app/protected/user/user.module.ts
+++ src/app/protected/user/user.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from "@angular/core";
-import { SharedModule } from "../../shared/shared.module";
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
 
-import { UserRoutingModule } from "./user-routing.module";
-import { InformationsComponent } from "./informations/informations.component";
-import { CommandesComponent } from "./commandes/commandes.component";
-import { FavorisComponent } from "./favoris/favoris.component";
-import { UserComponent } from "./user.component";
-import { InformationsModule } from "./informations/informations.module";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { UserRoutingModule } from './user-routing.module';
+import { InformationsComponent } from './informations/informations.component';
+import { CommandesComponent } from './commandes/commandes.component';
+import { FavorisComponent } from './favoris/favoris.component';
+import { UserComponent } from './user.component';
+import { InformationsModule } from './informations/informations.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
--- src/app/shared/services/api/users/users.service.ts
+++ src/app/shared/services/api/users/users.service.ts
@@ -1,17 +1,17 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { ApiService } from "../api.service";
-import { Observable } from "rxjs";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ApiService } from '../api.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class UsersService {
   constructor(private http: HttpClient) {}
 
   httpOptions = {
     headers: new HttpHeaders({
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     }),
   };
 
