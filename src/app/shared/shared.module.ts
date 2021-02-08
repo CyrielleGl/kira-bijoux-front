359c24a8fe916dc42dbc4d7f9221e23fd53e3a80
CyrielleGl <cyrielle.gallou@gmail.com>
2021-02-08 16:56:45 +0100

    add item-detail

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbDropdown, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ItemDetailComponent],
   imports: [
     CommonModule,
     NgbModule,
