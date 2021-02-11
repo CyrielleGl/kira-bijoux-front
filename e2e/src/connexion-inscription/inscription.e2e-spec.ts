526d4195d92f6705f1c2bce3c36a1d822efe6db1
CyrielleGl <cyrielle.gallou@gmail.com>
2021-02-11 13:04:08 +0100

    e2e connexion/inscription

--- e2e/src/connexion-inscription/inscription.e2e-spec.ts
+++ e2e/src/connexion-inscription/inscription.e2e-spec.ts
@@ -1,44 +1,25 @@
 import { browser, element, by, ExpectedConditions as ec } from 'protractor';
 
 import chai = require('chai');
-import { RoutingPageObjects } from './routing-page-objects';
+import { RoutingPageObjects } from '../routing/routing-page-objects';
+import { ConnexionPageObjects } from './connexion-page-objects';
 const expect = chai.expect;
 
 describe('routing test', () => {
 
   let routingPageObjects: RoutingPageObjects;
+  let connexionPageObjects: ConnexionPageObjects;
 
   before( async () => {
     await browser.get('/');
     browser.waitForAngularEnabled(false);
     routingPageObjects = new RoutingPageObjects();
+    connexionPageObjects = new ConnexionPageObjects();
   });
 
   it('should load home page', async () => {
     await browser.get('/home');
     await browser.sleep(500);
-  });
-
-  it('should open dropdown connexion/inscription', async () => {
-    await browser.wait(ec.visibilityOf(element(by.id('connexion-dropdown'))));
-    await browser.sleep(500);
-    await routingPageObjects.clickOnDropdownConnexion();
-    await browser.sleep(500);
-    const expected = 'connexion-link';
-    await browser.wait(ec.visibilityOf(element(by.id('connexion-link'))));
-    const value = await element(by.id('connexion-link')).getAttribute('id');
-    expect(value).to.eq(expected);
-    await browser.sleep(500);
-  });
-
-  it('should load connexion page', async () => {
-    await browser.wait(ec.visibilityOf(element(by.id('connexion-link'))));
-    await routingPageObjects.clickOnConnexionRoute();
-    await browser.sleep(500);
-    const expected = 'connexion';
-    await browser.wait(ec.visibilityOf(element(by.id('connexion'))));
-    const value = await element(by.id('connexion')).getAttribute('id');
-    expect(value).to.eq(expected);
   });
 
   it('should re-open dropdown connexion/inscription', async () => {
@@ -63,15 +44,13 @@ describe('routing test', () => {
     await browser.sleep(500);
   });
 
-  it('should load panier page', async () => {
-    await browser.wait(ec.visibilityOf(element(by.id('panier-link'))));
-    await routingPageObjects.clickOnPanierRoute();
+  it('should set new user data', async () => {
+    await connexionPageObjects.autoSignUpUser('selenium', 'e2e', 'selenium@selenium.com', 'selenium', 'selenium');
     await browser.sleep(500);
-    const expected = 'panier';
-    await browser.wait(ec.visibilityOf(element(by.id('panier'))));
-    const value = await element(by.id('panier')).getAttribute('id');
+    const expected = 'home';
+    await browser.wait(ec.visibilityOf(element(by.id('home'))));
+    const value = await element(by.id('home')).getAttribute('id');
     expect(value).to.eq(expected);
-    await browser.sleep(500);
   });
 
   after(async () => {
