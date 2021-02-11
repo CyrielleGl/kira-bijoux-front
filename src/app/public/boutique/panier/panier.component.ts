2b5cbeb93d18b010f3f20cbf9cec8debbbd40d13
Hugo Fief <hfief1806@gmail.com>
2021-02-11 23:01:27 +0100

    US panier terminé et fonctionnel => appel api working complete

--- src/app/public/boutique/panier/panier.component.ts
+++ src/app/public/boutique/panier/panier.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { ShopService } from 'src/app/shared/services/api/shop/shop.service';
 
 @Component({
   selector: 'app-panier',
@@ -6,9 +9,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./panier.component.scss']
 })
 export class PanierComponent implements OnInit {
+  shoppingCart: any;
+  idUser = 0;
+  arrayPrice: any = [];
+  subTotalPrice: any = 0;
+  totalPrice: any = 0;
 
-  constructor() { }
+  constructor(
+    private shopService: ShopService,
+    private cookieService: CookieService,
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getShoppingCartByUser();
+  }
 
+  getShoppingCartByUser(): void {
+    this.idUser = parseInt(this.cookieService.get('kira-bijoux-id'));
+    this.shopService.getShoppingCartByUser(this.idUser).subscribe(
+      (data: any[]) => {
+        this.shoppingCart = data;
+        console.log(this.shoppingCart);
+        this.shoppingCart.map((res: any) => {
+          if (res.quantity == 1) {
+            this.subTotalPrice += res.item.price;
+          }
+          else if (res.quantity > 1) {
+            this.subTotalPrice += res.item.price * res.quantity;
+          }
+        })
+        this.totalPrice = this.subTotalPrice + 6.90;
+      }
+    );
+  }
+
+  putItemToShoppingCart(itemId: number, quantity: number): void {
+    const formData = {
+      quantity: quantity
+    };
+    this.shopService.putItemToShoppingCart(itemId, formData).subscribe(
+      () => { document.location.reload(); }
+    );
+  }
+
+  deleteItemToShoppingCart(itemId: number): void {
+    this.shopService.deleteItemToShoppingCart(itemId).subscribe(
+      () => { document.location.reload(); }
+    );
+  }
+
+  redirectToBoutique(): void {
+    this.router.navigateByUrl('/boutique/colliers', { skipLocationChange: false }).then(() => {
+      this.router.navigate(['boutique']);
+      document.location.reload();
+    });
+  }
+
+  onSubmitValidation(totalPrice: number): void {
+    alert(`Vous me devez ${totalPrice} €`)
+  }
 }
---
+++ src/app/shared/services/api/shop/shop.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ApiService } from '../api.service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ShopService {
+  constructor(private http: HttpClient) {}
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+  };
+
+  getAllShoppingCart(): Observable<string[]> {
+    return this.http.get<string[]>(`${ApiService.ApiUrlShop}`);
+  }
+
+  getShoppingCartByUser(userId: number): Observable<string[]> {
+    return this.http.get<string[]>(`${ApiService.ApiUrlShop}/user/` + userId);
+  }
+
+  postItemToShoppingCart(formData: any): Observable<string[]> {
+    return this.http.post<string[]>(`${ApiService.ApiUrlShop}`, formData);
+  }
+
+  putItemToShoppingCart(itemId: number, formData: any): Observable<string[]> {
+    return this.http.put<string[]>(`${ApiService.ApiUrlShop}/` + itemId, formData);
+  }
+
+  deleteItemToShoppingCart(itemId: number): Observable<string[]> {
+    return this.http.delete<string[]>(`${ApiService.ApiUrlShop}/` + itemId);
+  }
+}
