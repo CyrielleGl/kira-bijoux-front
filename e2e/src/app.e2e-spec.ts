394dd0f1fc5110e2d5a1f0648d50b9c4b66750f5
CyrielleGl <cyrielle.gallou@gmail.com>
2021-02-12 09:23:09 +0100

    little review correction

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,6 +1,4 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
-import { expect } from 'chai';
 
 describe('workspace-project App', () => {
   let page: AppPage;
