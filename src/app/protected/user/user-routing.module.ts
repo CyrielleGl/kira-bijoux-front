3968b714a9f3e03327ee3349a04b18cd99b9356b
CyrielleGl <61991625+CyrielleGl@users.noreply.github.com>
2021-03-09 12:50:04 +0100

    itialize tabs and update routing (#36)

--- src/app/protected/user/user-routing.module.ts
+++ src/app/protected/user/user-routing.module.ts
@@ -4,16 +4,33 @@ import { RouterModule, Routes } from '@angular/router';
 import { CommandesComponent } from './commandes/commandes.component';
 import { FavorisComponent } from './favoris/favoris.component';
 import { InformationsComponent } from './informations/informations.component';
-
+import { UserComponent } from './user.component';
 
 const routes: Routes = [
-  { path: 'mon-compte/commandes', component: CommandesComponent },
-  { path: 'mon-compte/favoris', component: FavorisComponent },
-  { path: 'mon-compte/informations', component: InformationsComponent },
-  { path: 'mon-compte',
-  loadChildren: () => import('../user/user.module').then(m => m.UserModule)
+  {
+    path: '',
+    component: UserComponent,
+    children: [
+      {
+        path: 'commandes',
+        component: CommandesComponent
+      },
+      {
+        path: 'favoris',
+        component: FavorisComponent
+      },
+      {
+        path: 'informations',
+        component: InformationsComponent
+      },
+      {
+        path: '',
+        redirectTo: 'commandes',
+        pathMatch: 'full'
+      }
+    ]
   }
-  ];
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
