eec55f35b7ce95245d1822c5b88a5b873f25938d
Hugo Fief <hfief1806@gmail.com>
2021-05-08 01:06:55 +0200

    configuration progress spinner test need to fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
 import { AppConstants } from './shared/app-constants';
 import { UsersService } from './shared/services/api/users/users.service';
+import { LoaderService } from './shared/services/loader/loader.service';
 import { UtilsService } from './shared/services/utils/utils.service';
 
 @Component({
@@ -9,14 +11,27 @@ import { UtilsService } from './shared/services/utils/utils.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  public isLoadingSpinnerActive: boolean = false;
   yearNow = new Date().getFullYear().toString();
   appTitle = AppConstants.appTitle;
   urlLogoHeader = AppConstants.urlLogoHeader;
 
   constructor(
     private utilsService: UtilsService,
-    private usersService: UsersService
-  ) {}
+    private usersService: UsersService,
+    private loaderService: LoaderService,
+    private router: Router
+  ) {
+    this.loaderService.loading.subscribe((isVisible) => (this.isLoadingSpinnerActive = isVisible));
+
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationStart) {
+        this.isLoadingSpinnerActive = true;
+      } else if (event instanceof NavigationError || event instanceof NavigationEnd || event instanceof NavigationCancel) {
+        this.isLoadingSpinnerActive = false;
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.utilsService.initFaIcons();
