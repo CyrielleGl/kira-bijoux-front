7a1836e2ae3a3ecbc1e63ebd9b86da1819d45252
CyrielleGl <cyrielle.gallou@gmail.com>
2021-05-09 21:11:31 +0200

    WIP - dashboard init

--- src/app/protected/admin/dashboard/dashboard.component.ts
+++ src/app/protected/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/shared/models/user.model';
+import { UsersService } from 'src/app/shared/services/api/users/users.service';
 import { SecuService } from 'src/app/shared/services/secu/secu.service';
 
 @Component({
@@ -8,10 +10,23 @@ import { SecuService } from 'src/app/shared/services/secu/secu.service';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private cookieService: SecuService) { }
+  users: User[] | any = [];
+
+  constructor(
+    private cookieService: SecuService,
+    private usersService: UsersService
+    ) { }
 
   ngOnInit(): void {
     this.cookieService.verifyAccess('admin');
+    this.getAllUsers();
+  }
+
+  getAllUsers(): void {
+    this.usersService.getAllUsers().subscribe((data: string[]): void => {
+      this.users = data;
+      console.warn(this.users);
+    });
   }
 
 }
